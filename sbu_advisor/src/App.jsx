4bbfce9227dde926d806ba6b0e6eca8ba720c6aa
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-11-08 18:43:31 -0500

    fixed errors

--- sbu_advisor/src/App.jsx
+++ sbu_advisor/src/App.jsx
@@ -1,27 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import PlannerPage from './pages/PlannerPage';
+import "./App.css";
+import PlannerPage from "./pages/PlannerPage";
 import HomePage from "./pages/HomePage";
-import { Routes, Route } from "react-router-dom";
-
-
-
+import { Routes, Route, Router } from "react-router-dom";
 
 function App() {
   return (
-     <Router>
-      <div className='app-container'>
-        <div className="page-content">
-          <Routes>
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="/portal" element={<PlannerPage/>}/>
-          </Routes>
-        </div>
+    <div className="app-container">
+      <div className="page-content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/portal" element={<PlannerPage />} />
+        </Routes>
       </div>
-    </Router>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
