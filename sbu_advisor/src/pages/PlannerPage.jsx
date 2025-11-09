1254d8ce5d323b42bd449114deece9d5bc9236b1
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-11-08 20:42:31 -0500

    added eleven widget

--- sbu_advisor/src/pages/PlannerPage.jsx
+++ sbu_advisor/src/pages/PlannerPage.jsx
@@ -1,34 +1,45 @@
 // Import: Navigation
-import { useLocation } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 // Import: Components
-import ContentHeader from '../components/ContentHeader'
-import SideNav from '../components/SideNav'
-import MainView from '../components/MainView'
-import SemesterBoard from '../components/SemesterBoard'
+import ContentHeader from "../components/ContentHeader";
+import SideNav from "../components/SideNav";
+import MainView from "../components/MainView";
+import SemesterBoard from "../components/SemesterBoard";
 
 // Import: CSS
-import './PlannerPage.css'
+import "./PlannerPage.css";
 
 // Import: Predefine semesters data
-import planSemesters from '../data/avaliable_semesters.json'
+import planSemesters from "../data/avaliable_semesters.json";
 
 export default function Portal() {
-  const { state } = useLocation()
-  const userName = state?.name || 'Guest'
+  const { state } = useLocation();
+  const userName = state?.name || "Guest";
 
   const handleNewSemester = () => {
-    {/* Send Form Data */}
-  }
+    {
+      /* Send Form Data */
+    }
+  };
 
   const handleSaveRoadmap = () => {
     // Persist the current plan (placeholder: static JSON for now)
-    localStorage.setItem('roadmap', JSON.stringify({ semesters: planSemesters }))
-  }
+    localStorage.setItem(
+      "roadmap",
+      JSON.stringify({ semesters: planSemesters })
+    );
+  };
 
   return (
     <div className="portal-page">
+      <elevenlabs-convai agent-id="agent_2001k9gndv75fw5at9cwr5m29k7x"></elevenlabs-convai>
+      <script
+        src="https://unpkg.com/@elevenlabs/convai-widget-embed"
+        async
+        type="text/javascript"
+      ></script>
       <ContentHeader
         onNewSemester={handleNewSemester}
         onSaveRoadmap={handleSaveRoadmap}
@@ -42,5 +53,5 @@ export default function Portal() {
         </MainView>
       </div>
     </div>
-  )
+  );
 }
